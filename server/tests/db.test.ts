a93967bc89d722860fb1f38af8e18435e8bc0a61
Matan Swisa <matanswisa01@gmail.com>
2022-07-17 23:16:39 +0300

    add jest and basic tests

---
+++ server/tests/db.test.ts
@@ -0,0 +1,11 @@
+
+describe('check a simple test', () => {
+    // it('this suppose to fail', () => {
+    //     expect(5 + 5).toBe(2);
+    // })
+    it('this is suppose to work', () => {
+        expect(5 + 5).toBe(10);
+    })
+})
+
+
